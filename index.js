013a098a8ba28662a8730c2b14ab9e6311b7caa4
Andrea <andreamayhirji@gmail.com>
2018-10-24 16:33:46 +0000

    refactored to use a getter

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 class Account {
   constructor(username) {
     this.username = username;
@@ -12,21 +11,24 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
 //subclass of Transaction
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 }
 
 //subclass of Transaction
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 
 }
@@ -36,18 +38,22 @@ class Withdrawal extends Transaction {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-const myAccount = new Account('snow-patrol');
+const myAccount = new Account('Andrea');
 
-t1 = new Withdrawal(50.25, myAccount);
+console.log('Balance:', myAccount.balance)
+
+t1 = new Deposit(300.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
+t3 = new Deposit(12.00, myAccount);
 t3.commit();
-console.log('Transaction 3:', t3)
 
-console.log('Balance:', myAccount.balance);
+console.log('Current balance:', myAccount.balance);
+
+t4 = new Withdrawal(630.00, myAccount);
+t4.commit();
+
+console.log('New balance', myAccount.balance);
